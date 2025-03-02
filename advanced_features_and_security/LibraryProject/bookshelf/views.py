1792d8003373f5df6dda220d53ffbe1ad2a170c3
Sumeya Abdulsemed <sumeyaabdu9@gmail.com>
2025-03-02 23:27:34 +0300

    second

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book  # Assuming you have a Book model
+from django.http import HttpResponseForbidden
 
-# Create your views here.
+# Example view to list books (only accessible by users with 'can_view' permission)
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()  # Get all books
+    return render(request, 'bookshelf/book_list.html', {'books': books})
