6df54f36748149010b698368f2d9d4376811463d
Sumeya Abdulsemed <sumeyaabdu9@gmail.com>
2025-03-02 23:00:17 +0300

    try

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,17 +1,8 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.contrib.auth.models import User
 from .managers import CustomUserManager
-
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.username
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -21,14 +12,37 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        """
+        Create and return a regular user with a username and password.
+        """
+        if not username:
+            raise ValueError('The Username must be set')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)  # Hash the password
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        """
+        Create and return a superuser with a username, password, and admin rights.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, password, **extra_fields)
+    
 class CustomUser(AbstractUser):
-    # Add the custom fields
+    # Additional custom fields
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
+    objects = CustomUserManager()  # Use the custom manager
+
     def __str__(self):
         return self.username
-    
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
