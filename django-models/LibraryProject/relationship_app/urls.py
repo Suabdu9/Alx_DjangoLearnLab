534b8d2dcf23c9ef0ac4e6f6b9b8eb3e31e762f6
Sumeya Abdulsemed <sumeyaabdu9@gmail.com>
2025-03-02 06:58:28 +0300

    third

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
 
     # Class-based view for library details
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('home/', views.home, name='home'), 
 ]
