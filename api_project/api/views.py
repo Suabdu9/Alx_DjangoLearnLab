09043b2dec645caa5c6690874bca6c44ad4bb3e9
Sumeya Abdulsemed <sumeyaabdu9@gmail.com>
2025-04-03 17:08:19 +0300

    api third

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.viewsets import ModelViewSet
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
