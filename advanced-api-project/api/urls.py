74a60b4885d1f44360ea535d017e8812894c2b7a
Sumeya Abdulsemed <sumeyaabdu9@gmail.com>
2025-04-06 18:55:09 +0300

    advanced api 1

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import AuthorListCreateView, BookListCreateView
+from .views import (
+    BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView, AuthorListCreateView, BookListCreateView
+)
 
 urlpatterns = [
     path('authors/', AuthorListCreateView.as_view(), name='author-list-create'),
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete') ,
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import generics
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
@@ -10,3 +11,33 @@ class AuthorListCreateView(generics.ListCreateAPIView):
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    def perform_create(self, serializer):
+        serializer.save()
+    permission_classes = [IsAuthenticated]
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    def perform_update(self, serializer):
+        serializer.save()
+    permission_classes = [IsAuthenticated]
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
