50270c3734b2a95d820cb4f5280b9f290c382982
Sumeya Abdulsemed <sumeyaabdu9@gmail.com>
2025-04-06 18:59:12 +0300

    advanced api 1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
 from rest_framework import generics
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
