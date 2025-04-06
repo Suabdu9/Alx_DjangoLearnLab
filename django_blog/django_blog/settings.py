99e81d7f1820c191ca9c6f629f6d52d1db749b0d
Sumeya Abdulsemed <sumeyaabdu9@gmail.com>
2025-04-06 22:51:52 +0300

    django

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -85,6 +85,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'your_database_user',
+        'HOST': 'localhost',  # or another host
+        'PORT': '5432',    
     }
 }
 
